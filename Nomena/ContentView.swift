00c2d2f3cf546af9f2bfcbc27df1787cc4b0f741
Max <barreiro.fontela.max@gmail.com>
2026-01-18 22:54:57 +0200

    Initial Commit

---
+++ Nomena/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Nomena
+//
+//  Created by MaxTop4ik022 on 18.01.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Nomena/NomenaApp.swift
@@ -0,0 +1,17 @@
+//
+//  NomenaApp.swift
+//  Nomena
+//
+//  Created by MaxTop4ik022 on 18.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct NomenaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
