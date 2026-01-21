7d7c7e2ead9178cc448ec9396f39174b3521c516
Max <barreiro.fontela.max@gmail.com>
2026-01-21 15:52:38 +0200

    [Refactor: RandomNameScreen] Change property names to private(set) in RandomNameViewModel

--- Nomena/Features/RandomNameScreen/ViewModels/RandomNameViewModel.swift
+++ Nomena/Features/RandomNameScreen/ViewModels/RandomNameViewModel.swift
@@ -8,7 +8,7 @@ import Observation
     private let repository: JSONRepository
     
     @ObservationIgnored
-    private var names: [String]
+    private(set) var names: [String]
     
     init(
         currentCard: NamedCard,
