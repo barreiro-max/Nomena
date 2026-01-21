d42b3cf89044f62cb1551edfc7fedf7206b82d61
Max <barreiro.fontela.max@gmail.com>
2026-01-21 17:33:19 +0200

    [Test: RandomNameScreen] Cover RandomNameViewModel with tests

---
+++ NomenaTests/RandomNameScreenTests/ViewModelsTests/RandomNameViewModelTests.swift
@@ -0,0 +1,48 @@
+import Testing
+@testable import Nomena
+
+@MainActor
+struct RandomNameViewModelTests {
+    private let viewModel: RandomNameViewModel
+    
+    init() {
+        let mock = MockLocalJSONRepository()
+        let loader = NamesLoader(repository: mock)
+        viewModel = RandomNameViewModel(
+            currentCard: .init(name: "TestCard"),
+            loader: loader,
+            names: []
+        )
+    }
+    
+    @Test func viewModel_loadNames_expectedSixNames_fromMockRepo() async {
+        viewModel.loadNames()
+        #expect(viewModel.names.count == 6)
+    }
+    
+    @Test func viewModel_loadNames_expectedErrorMessage_equalNil() {
+        viewModel.loadNames()
+        #expect(viewModel.errorMessage == nil)
+    }
+    
+    @Test func viewModel_loadNames_expectedIsLoading_equalFalse() {
+        viewModel.loadNames()
+        #expect(!viewModel.isLoading)
+    }
+    
+    @Test func viewModel_changeCard_currentCardDoesntHaveName_whenNamesNotLoaded() throws {
+        viewModel.changeCard()
+        
+        let card = try #require(viewModel.currentCard)
+        #expect(card.name == "Імʼя відсутнє")
+
+    }
+
+    @Test func viewModel_changeCard_setsCurrentCard_nonNil() {
+        viewModel.loadNames()
+        
+        viewModel.changeCard()
+        
+        #expect(viewModel.currentCard != nil)
+    }
+}
