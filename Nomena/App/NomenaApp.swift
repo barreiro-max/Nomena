f85e08e171ea9af72adbbe9921457db1c0502756
Max <barreiro.fontela.max@gmail.com>
2026-02-05 15:16:57 +0200

    [Arcitecture] Introduce NomenaApp as app entry with AppDependencies state

--- Nomena/App/NomenaApp.swift
+++ Nomena/App/NomenaApp.swift
@@ -9,9 +9,12 @@ import SwiftUI
 
 @main
 struct NomenaApp: App {
+    @State(initialValue: AppDependencies()) private var dependencies
+    
     var body: some Scene {
         WindowGroup {
             NomenaView()
+                .environment(\.dependencies, dependencies)
         }
     }
 }
