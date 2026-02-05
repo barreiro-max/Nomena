79d580b01bd013d1fc9b74f8d3323c59a0df77fc
Max <barreiro.fontela.max@gmail.com>
2026-02-05 15:23:51 +0200

    [UI: RandomNameScreen] Add card stack logic and basic layout to NomenaView

--- Nomena/App/NomenaView.swift
+++ Nomena/App/NomenaView.swift
@@ -8,17 +8,42 @@
 import SwiftUI
 
 struct NomenaView: View {
+    @Environment(\.dependencies) private var dependencies
+    @State var viewModel: RandomNameViewModel?
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(spacing: 48) {
+            ZStack {
+                ForEach(
+                    0...(viewModel?.names.count ?? 0),
+                    id: \.self
+                ) { _ in
+                    SwipeCardView(title: viewModel?.randomCardName() ?? "Імʼя відсутнє")
+                }
+            }
         }
-        .padding()
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(
+            .linearGradient(
+                colors: [
+                    .white,
+                    .bottomMainBackground,
+                    .bottomMainBackground
+                ],
+                startPoint: .top,
+                endPoint: .bottom
+            ),
+            ignoresSafeAreaEdges: .all
+        )
+        .task {
+            viewModel = dependencies?.makeViewModel()
+            await viewModel?.loadNames()
+        }
     }
 }
 
 #Preview {
+    let dependencies = AppDependencies()
     NomenaView()
+        .environment(\.dependencies, dependencies)
 }
